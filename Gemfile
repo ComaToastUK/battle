8d050a52f39a95f208e79279d9d2488013446ceb
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-09-12 14:33:43 +0100

    created testing framework and file structure

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+ruby '2.4.0'
+
+gem 'selenium-webdriver'
+gem 'capybara'
+gem 'rspec'
+gem 'shotgun'
+gem 'thin'
+gem 'sinatra'
---
+++ spec/app_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe Battle do
+
+  # As two Players,
+  # So we can play a personalised game of Battle,
+  # We want to Start a fight by entering our Names and seeing them
+
+  it 'allows 2 players to enter their names' do
+    expect(subject.name).to eq 'James'
+  end
+end
